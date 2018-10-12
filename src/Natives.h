46b66afc11100db14d820cd1904d68504eab2c75
Romz24 <romz24@mail.ru>
2018-10-12 17:05:36 +0300

    Update Natives.h

--- src/Natives.h
+++ src/Natives.h
@@ -12,7 +12,7 @@ public:
 	static cell AMX_NATIVE_CALL TextDrawSetStringf(AMX* amx, cell* params);
 	static cell AMX_NATIVE_CALL PlayerTextDrawSetStringf(AMX* amx, cell* params);
 	static cell AMX_NATIVE_CALL ShowPlayerDialogf(AMX* amx, cell* params);
-	static cell AMX_NATIVE_CALL SendPlayerMessageToPlayer(AMX* amx, cell* params);
-	static cell AMX_NATIVE_CALL SendPlayerMessageToAll(AMX* amx, cell* params);
-	static cell AMX_NATIVE_CALL SetPlayerChatBubble(AMX* amx, cell* params);
+	static cell AMX_NATIVE_CALL SendPlayerMessageToPlayerf(AMX* amx, cell* params);
+	static cell AMX_NATIVE_CALL SendPlayerMessageToAllf(AMX* amx, cell* params);
+	static cell AMX_NATIVE_CALL SetPlayerChatBubblef(AMX* amx, cell* params);
 };
